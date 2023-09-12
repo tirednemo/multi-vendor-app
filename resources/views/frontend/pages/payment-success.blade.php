d3c7541e672cfe340fc37616aa2c294f20d2bdfd
tirednemo <ummetasnim@iut-dhaka.edu>
2023-09-13 01:34:20 +0600

    Payment success before payment in COD fixed

--- resources/views/frontend/pages/payment-success.blade.php
+++ resources/views/frontend/pages/payment-success.blade.php
@@ -6,8 +6,8 @@
 
 @section('content')
     <!--============================
-        BREADCRUMB START
-    ==============================-->
+            BREADCRUMB START
+        ==============================-->
     <section id="wsus__breadcrumb">
         <div class="wsus_breadcrumb_overlay">
             <div class="container">
@@ -15,7 +15,7 @@
                     <div class="col-12">
                         <h4>payment</h4>
                         <ul>
-                            <li><a href="{{route('home')}}">home</a></li>
+                            <li><a href="{{ route('home') }}">home</a></li>
                             <li><a href="javascript:;">payment</a></li>
                         </ul>
                     </div>
@@ -24,23 +24,29 @@
         </div>
     </section>
     <!--============================
-        BREADCRUMB END
-    ==============================-->
+            BREADCRUMB END
+        ==============================-->
 
 
     <!--============================
-        PAYMENT PAGE START
-    ==============================-->
+            PAYMENT PAGE START
+        ==============================-->
     <section id="wsus__cart_view">
         <div class="container">
             <div class="wsus__pay_info_area">
-                <div class="row">
-                    <h1>Paymet success!</h1>
-                </div>
+                @if (session('method') == 'cod')
+                    <div class="row">
+                        <h1>Order placed!</h1>
+                    </div>
+                @else
+                    <div class="row">
+                        <h1>Payment success!</h1>
+                    </div>
+                @endif
             </div>
         </div>
     </section>
     <!--============================
-        PAYMENT PAGE END
-    ==============================-->
+            PAYMENT PAGE END
+        ==============================-->
 @endsection
